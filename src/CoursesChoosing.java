fb4ec52dcbbae60e98ad37b4cb165cacc3d0fe90
zhaiyunfan20000324@qq.com <30565458+zhaiyunfan@users.noreply.github.com>
2020-11-24 11:32:48 +0800

    update class2

--- src/CoursesChoosing.java
+++ src/CoursesChoosing.java
@@ -2,12 +2,51 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class CoursesChoosing
 {
     public static void main(String[] args)
     {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Student[] students = new Student[n];
+        for (int i = 0; i < n; i++)
+        {
+            String name;
+            int grade;
 
+            name = scanner.next();
+            grade = scanner.nextInt();
+
+            int courseNum;
+
+            courseNum = scanner.nextInt();
+
+            Course[] courses = new Course[courseNum];
+            String courseName;
+            int courseHours;
+            for (int j = 0; j < courseNum; j++)
+            {
+                courseName = scanner.next();
+                courseHours = scanner.nextInt();
+
+                courses[j] = new Course(courseName,courseHours);
+            }
+            students[i] = new Student(name,grade,courses);
+        }
+        Printer printer = new Printer();
+        for (int i = 0; i < n; i++)
+        {
+            try
+            {
+                printer.print("第"+(i+1)+"学生信息如下:\n");
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            students[i].showMessage();
+        }
     }
 }
 
@@ -80,7 +119,20 @@ class Student
 
     public void showMessage()
     {
-        System.out.print(name+" "+grade+"年级");
+        Printer printer = new Printer();
+        try
+        {
+            printer.print(name+" "+grade+"年级\n");
+            printer.print("选修的课程包括:\n");
+            for (int i = 0; i < courses.length; i++)
+            {
+                printer.print("("+(i+1)+")"+courses[i].getCourseName()+"\t"+courses[i].getCourseHour()+"分\n");
+            }
+            printer.print("总学分:"+this.getHours()+"\n");
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
 
